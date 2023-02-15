541e8af1b646e7ce70961f9d2a3581bab56317bc
bowseruk <13262235+bowseruk@users.noreply.github.com>
2023-02-15 22:13:42 +0000

    Fixed movie name

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,3 +1,2 @@
-const oldmoviesList = ["Titanic","The Rock","Face Off","Inception","Interstella","Goodfellas","The Dark Knight","Jaws","Pulp Fiction","The Truman Show","The Exorcist","Forrest Gump","The Running Man"];
-const badMovies = ["Lockstock","w"]
-const moviesList = ["Titanic","The Rock","Face Off","Inception","Interstella"];
+const moviesList = ["Titanic","The Rock","Face/Off","Inception","Interstellar","Goodfellas","The Dark Knight","Jaws","Pulp Fiction","The Truman Show","The Exorcist","Forrest Gump","The Running Man"];
+const badMovies = ["Lockstock","w"];
